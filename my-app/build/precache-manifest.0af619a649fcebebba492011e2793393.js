37a08977a89d598e8f9b5e4269e1a3f5f59b4a6f
Michael Askndafi <maskndafi@gmail.com>
2020-07-19 13:13:43 -0700

    Idk Anymore

---
+++ my-app/build/precache-manifest.0af619a649fcebebba492011e2793393.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bb359a2b19577bc2efa55a351cfd9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b3f9636128ce64222e7",
+    "url": "/static/css/2.dc432183.chunk.css"
+  },
+  {
+    "revision": "7e153a9e35fa1b46ddf4",
+    "url": "/static/css/main.1f59809a.chunk.css"
+  },
+  {
+    "revision": "8b3f9636128ce64222e7",
+    "url": "/static/js/2.05dfafa1.chunk.js"
+  },
+  {
+    "revision": "da3a1ff13b6584de816969518cbca7dc",
+    "url": "/static/js/2.05dfafa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e153a9e35fa1b46ddf4",
+    "url": "/static/js/main.4450aa40.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
